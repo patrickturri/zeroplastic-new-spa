e933db2199b0f5756a3cde26cebcf607eb58d46d
Turricelli <patrickturricelli8@github.com>
2020-09-19 16:49:04 +0200

    Add FAQ

--- components/Features.vue
+++ components/Features.vue
@@ -1,15 +1,9 @@
 <template>
   <section class="bg-white border-b py-8">
     <div class="container max-w-5xl mx-auto m-8">
-      <h1
-        class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800"
-      >
-        Title
-      </h1>
+      <h1 class="w-full my-2 text-5xl font-bold leading-tight text-left text-gray-800">Feautures</h1>
       <div class="w-full mb-4">
-        <div
-          class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"
-        ></div>
+        <div class="h-3 mx-auto gradient w-128 opacity-25 my-0 py-0 rounded-t"></div>
       </div>
       <feature-right
         headline="Lorem ipsum dolor sit amet"
@@ -18,8 +12,8 @@
         <img src="~assets/img/travel-booking.svg?inline" alt slot="svg" />
       </feature-right>
       <feature-left
-        headline="Lorem ipsum dolor sit amet"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula."
+        headline="Transparent Dashboard"
+        content="Know where the plastic comes from and anomaly detection discovers failure in the process."
       >
         <img src="~assets/img/connected-world.svg?inline" alt slot="svg" />
       </feature-left>
@@ -70,14 +64,14 @@
 </template>
 
 <script>
-import FeatureRight from '@/components/FeatureRight'
-import FeatureLeft from '@/components/FeatureLeft'
+import FeatureRight from "@/components/FeatureRight";
+import FeatureLeft from "@/components/FeatureLeft";
 
 export default {
-  name: 'Features',
+  name: "Features",
   components: {
-    'feature-right': FeatureRight,
-    'feature-left': FeatureLeft
+    "feature-right": FeatureRight,
+    "feature-left": FeatureLeft
   }
-}
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,17 @@ export default {
 	 ** Headers of the page
 	 */
 	head: {
-	  title: process.env.npm_package_name || '',
-	  meta: [
-		{ charset: 'utf-8' },
-		{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-		{
-		  hid: 'description',
-		  name: 'description',
-		  content: process.env.npm_package_description || ''
-		}
-	  ],
-	  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: process.env.npm_package_description || ''
+			}
+		],
+		link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ]
 	},
 	/*
 	 ** Customize the progress-bar color
@@ -32,31 +32,31 @@ export default {
 	 ** Nuxt.js dev-modules
 	 */
 	buildModules: [
-	  // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-	  '@nuxtjs/tailwindcss'
+		// Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+		'@nuxtjs/tailwindcss'
 	],
 	/*
 	 ** Nuxt.js modules
 	 */
-	modules: ['nuxt-svg-loader'],
+	modules: [ 'nuxt-svg-loader' ],
+
 	purgeCSS: {
-	  whitelist: ['hidden'],
-	  whitelistPatterns: [/md:w-[1-6]/]
+		whitelist: [ 'hidden' ],
+		whitelistPatterns: [ /md:w-[1-6]/ ]
 	},
 	/*
 	 ** Build configuration
 	 */
 	build: {
-	  /*
+		/*
 	   ** You can extend webpack config here
 	   */
-	  extend(config, ctx) {
-		loaders: {
-		  file: {
-			esModule: false
-		  }
+		extend(config, ctx) {
+			loaders: {
+				file: {
+					esModule: false;
+				}
+			}
 		}
-	  }
 	}
-  }
-  
+};
