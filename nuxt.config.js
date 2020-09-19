8c7b4a8f19d984a2733e3082bc4bc33847cff898
Antonio Terpin <antonio.terping@gmail.com>
2020-09-19 13:53:02 +0200

    funziona.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,62 @@
 export default {
-	// Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-	ssr: false,
-
-	// Global page headers (https://go.nuxtjs.dev/config-head)
+	mode: 'universal',
+	/*
+	 ** Headers of the page
+	 */
 	head: {
-		title: 'ZeroPlastic',
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' }
-		],
-		link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ]
+	  title: process.env.npm_package_name || '',
+	  meta: [
+		{ charset: 'utf-8' },
+		{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+		{
+		  hid: 'description',
+		  name: 'description',
+		  content: process.env.npm_package_description || ''
+		}
+	  ],
+	  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
 	},
-
-	// Global CSS (https://go.nuxtjs.dev/config-css)
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: '#fff' },
+	/*
+	 ** Global CSS
+	 */
 	css: [],
-
-	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	/*
+	 ** Plugins to load before mounting the App
+	 */
 	plugins: [],
-
-	// Auto import components (https://go.nuxtjs.dev/config-components)
-	components: true,
-
-	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-	buildModules: [],
-
-	// Modules (https://go.nuxtjs.dev/config-modules)
-	modules: [
-		// https://go.nuxtjs.dev/bootstrap
-		'bootstrap-vue/nuxt',
-		// https://go.nuxtjs.dev/axios
-		'@nuxtjs/axios',
-		[
-			'nuxt-gmaps',
-			{
-				key: 'AIzaSyDKhKfkQFFJcAOgvJwZy59Z6_io0FKKNSQ'
-			}
-		]
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [
+	  // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+	  '@nuxtjs/tailwindcss'
 	],
-
-	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
-	axios: {},
-
-	// Build Configuration (https://go.nuxtjs.dev/config-build)
-	build: {}
-};
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: ['nuxt-svg-loader'],
+	purgeCSS: {
+	  whitelist: ['hidden'],
+	  whitelistPatterns: [/md:w-[1-6]/]
+	},
+	/*
+	 ** Build configuration
+	 */
+	build: {
+	  /*
+	   ** You can extend webpack config here
+	   */
+	  extend(config, ctx) {
+		loaders: {
+		  file: {
+			esModule: false
+		  }
+		}
+	  }
+	}
+  }
+  
