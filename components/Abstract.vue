e09b8ca7d9fc077513c35028fa31d738d1cd7ab9
Turricelli <patrickturricelli8@github.com>
2020-09-19 20:12:28 +0200

    change images

--- components/Abstract.vue
+++ components/Abstract.vue
@@ -1,107 +1,110 @@
 <template>
   <section class="bg-white border-b py-8">
     <div class="pt-24">
-        <div
-        class="container px-3 mx-auto flex flex-wrap flex-row md:flex-col items-center"
-        >
-            <div class="flex flex-wrap flex-col md:flex-row items-center mt-20 mb-20">
-                <div class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300">
-                    <img src="~/assets/img/green-tea.png" style="object-fit:cover;border-radius:50%;"/>
-                </div>
-                <div class="pointer m-2"></div>
-                <div class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300">
-                    <img src="~/assets/img/smiling-people.png" style="object-fit:cover;border-radius:50%;"/>
-                </div>
-                <div class="pointer m-2"></div>
-                <div class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300">
-                    <img src="~/assets/img/earth.png" style="object-fit:cover;"/>
-                </div>
-            </div>
-        <div
-            class="flex flex-col w-full md:w-4/5 justify-center items-start text-center md:text-left mt-20"
-        >
-            <ol class="text-black">
-                <li class="leading-normal text-4xl mb-20 flex flex-col">
-                    <div class="flex flex-row">
-                        <span
-                            class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black">
-                        1</span>
-                        <div class="self-center">Make your brand plastic free</div>
-                    </div>
-                    <div class="w-4xl self-center items-center justify-center m-10">
-                        <bar-chart class='leading-normal'/>
-                    </div>
-                </li>
-                <li class="leading-normal text-4xl mb-20 flex flex-col content-center justify-center">
-                    <div class="flex flex-row">
-                        <span 
-                            class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black">
-                        2</span>
-                        <div class="self-center">As a community, earn money while improving your green status</div>
-                    </div>
-                    <img src='~/assets/img/recognition.jpeg' class='w-1/2 m-5 self-center'/>
-                    
-                </li>
-                <li class="leading-normal text-4xl mb-20 flex flex-col">
-                    <div class="flex flex-row">
-                        <span 
-                            class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black">
-                        3</span>
-                        <div class="self-center">Let's clean the world together!</div>
-                    </div>
-                    <div class="leading-normal flex flex-row m-10">
-                        <img src='~/assets/img/plastic_1.jpg' class='w-1/3 m-5'/>
-                        <img src='~/assets/img/plastic_2.jpg' class='w-1/3 m-5'/>
-                        <img src='~/assets/img/plastic_3.jpg' class='w-1/3 m-5'/>
-                    </div>
-                    
-                </li>
-            </ol>
+      <div class="container px-3 mx-auto flex flex-wrap flex-row md:flex-col items-center">
+        <div class="flex flex-wrap flex-col md:flex-row items-center mt-20 mb-20">
+          <div
+            class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300"
+          >
+            <img src="~/assets/img/green-tea.png" style="object-fit:cover;border-radius:50%;" />
+          </div>
+          <div class="pointer m-2"></div>
+          <div
+            class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300"
+          >
+            <img src="~/assets/img/smiling-people.png" style="object-fit:cover;border-radius:50%;" />
+          </div>
+          <div class="pointer m-2"></div>
+          <div
+            class="rounded-full w-64 h-64 flex items-center justify-center m-5 border-2 border-gray-300"
+          >
+            <img src="~/assets/img/world.png" style="object-fit:cover;" />
+          </div>
         </div>
+        <div
+          class="flex flex-col w-full md:w-4/5 justify-center items-start text-center md:text-left mt-20"
+        >
+          <ol class="text-black">
+            <li class="leading-normal text-4xl mb-20 flex flex-col">
+              <div class="flex flex-row">
+                <span
+                  class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black"
+                >1</span>
+                <div class="self-center">Make your brand plastic free</div>
+              </div>
+              <div class="w-4xl self-center items-center justify-center m-10">
+                <bar-chart class="leading-normal" />
+              </div>
+            </li>
+            <li class="leading-normal text-4xl mb-20 flex flex-col content-center justify-center">
+              <div class="flex flex-row">
+                <span
+                  class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black"
+                >2</span>
+                <div
+                  class="self-center"
+                >As a community, earn money while improving your green status</div>
+              </div>
+              <img src="~/assets/img/recognition.jpeg" class="w-1/2 m-5 self-center" />
+            </li>
+            <li class="leading-normal text-4xl mb-20 flex flex-col">
+              <div class="flex flex-row">
+                <span
+                  class="rounded-full w-12 h-12 flex items-center justify-center mr-20 border-2 border-black"
+                >3</span>
+                <div class="self-center">Let's clean the world together!</div>
+              </div>
+              <div class="leading-normal flex flex-row m-10">
+                <img src="~/assets/img/plastic_1.jpg" class="w-1/3 m-5" />
+                <img src="~/assets/img/plastic_2.jpg" class="w-1/3 m-5" />
+                <img src="~/assets/img/plastic_3.jpg" class="w-1/3 m-5" />
+              </div>
+            </li>
+          </ol>
         </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-
-import BarChart from '@/components/BarChart'
+import BarChart from "@/components/BarChart";
 
 export default {
-  name: 'Abstract',
+  name: "Abstract",
   components: {
-    'bar-chart': BarChart
+    "bar-chart": BarChart
   }
-}
+};
 </script>
 
 <style>
-    .pointer {
-      width: 100px;
-      height: 40px;
-      position: relative;
-      background: #3a85ff88;
-    }
-    .pointer:after {
-      content: "";
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 0;
-      height: 0;
-      border-left: 20px solid white;
-      border-top: 20px solid transparent;
-      border-bottom: 20px solid transparent;
-    }
-    .pointer:before {
-      content: "";
-      position: absolute;
-      right: -20px;
-      bottom: 0;
-      width: 0;
-      height: 0;
-      border-left: 20px solid #3a85ff88;
-      border-top: 20px solid transparent;
-      border-bottom: 20px solid transparent;
-    }
+.pointer {
+  width: 100px;
+  height: 40px;
+  position: relative;
+  background: #3a85ff88;
+}
+.pointer:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-left: 20px solid white;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+}
+.pointer:before {
+  content: "";
+  position: absolute;
+  right: -20px;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-left: 20px solid #3a85ff88;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <hero />
-    <abstract/>
+    <abstract />
     <features />
-    <faq/>
+    <faq />
   </div>
 </template>
 
 <script>
-import Hero from '@/components/Hero'
-import Abstract from '@/components/Abstract'
-import Features from '@/components/Features'
-import Teasers from '@/components/Teasers'
-import Prices from '@/components/Prices'
-import Faq from '@/components/Faq'
+import Hero from "@/components/Hero";
+import Abstract from "@/components/Abstract";
+import Features from "@/components/Features";
+import Teasers from "@/components/Teasers";
+import Prices from "@/components/Prices";
+import Faq from "@/components/Faq";
 
 export default {
-  name: 'LandingPage',
+  name: "LandingPage",
   components: {
     hero: Hero,
     abstract: Abstract,
     features: Features,
     teasers: Teasers,
     prices: Prices,
-    'faq': Faq
+    faq: Faq
   }
-}
+};
 </script>
