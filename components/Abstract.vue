39876b5d6153c0d9d337b3145d37ffe9ac9c8a78
Antonio Terpin <antonio.terping@gmail.com>
2020-09-19 13:29:43 +0200

    Merge done

---
+++ components/Abstract.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="bg-white border-b py-8">
+    <div class="pt-24">
+        <div
+        class="container px-3 mx-auto flex flex-wrap flex-row md:flex-col items-center"
+        >
+        <div class="w-full md:w-3/5 py-6 text-center">
+            <img class="w-full md:w-4/5 z-50" src="~/assets/img/hero.png" alt />
+        </div>
+        <div
+            class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
+        >
+            <ol class="text-black">
+                <li class="leading-normal text-2xl mb-8">Make your brand plastic free</li>
+                <li class="leading-normal text-2xl mb-8">As a community, earn money while improving your green status</li>
+                <li class="leading-normal text-2xl mb-8">Let's clean the world together!</li>
+
+            </ol>
+        </div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Abstract'
+}
+</script>
